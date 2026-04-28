edd44833bb633b20b9e9aa9fd7c7d3050c4c3e54
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-29 00:42:02 +0600

    logicaly social login section hide

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,36 +1,49 @@
-"use client"
-import { authClient } from '@/lib/auth-client';
+"use client";
+import { authClient } from "@/lib/auth-client";
 
-
-import { Button } from '@heroui/react';
-import Link from 'next/link';
-import React from 'react';
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import React from "react";
 import { FaGoogle } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const SocialLogin = () => {
-    const googleLogin = async()=>{
-        const data = await authClient.signIn.social({
-            provider:"google"
-        })
-        console.log('data', data)
-
-    }
-    return (
-        <div className='flex flex-col space-y-4'>
-            <h2 className='font-bold text-2xl '>Login With </h2>
-            <div className='flex flex-col space-y-3 '>
-                <Button variant='outline' onClick={googleLogin} className='w-full flex justify-center items-center space-x-2 rounded-2xl py-2 border-2  border-blue-700'>
-                    <FaGoogle/>
-                    <span>Login with Google</span>
-                </Button>
-                <Link href={`/login`} className='w-full flex justify-center items-center space-x-2 rounded-2xl py-2 border-2  border-blue-700'>
-                    <MdEmail />
-                    <span>Login with Email</span>
-                </Link>
-            </div>
-        </div>
-    );
-}
+  const googleLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+    console.log("data", data);
+  };
+  const data =  authClient.useSession() 
+  const userData = data?.data?.user
+  if(userData){
+    return(
+        <>
+        </>
+    )
+  }
+  return (
+    <div className="flex flex-col space-y-4">
+      <h2 className="font-bold text-2xl ">Login With </h2>
+      <div className="flex flex-col space-y-3 ">
+        <Button
+          variant="outline"
+          onClick={googleLogin}
+          className="w-full flex justify-center items-center space-x-2 rounded-2xl py-2 border-2  border-blue-700"
+        >
+          <FaGoogle />
+          <span>Login with Google</span>
+        </Button>
+        <Link
+          href={`/login`}
+          className="w-full flex justify-center items-center space-x-2 rounded-2xl py-2 border-2  border-blue-700"
+        >
+          <MdEmail />
+          <span>Login with Email</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default SocialLogin;
--- src/proxy.js
+++ src/proxy.js
@@ -2,14 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 export async function proxy(request) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
-    if(!session) {
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-    return NextResponse.next();
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const pathname = request.nextUrl.pathname
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+//   if (session && (pathname === "/login" || pathname === "/register")) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+  return NextResponse.next();
 }
 export const config = {
-  matcher: ["/news/:id"], // Specify the routes the middleware applies to
+  matcher: ["/news/:path"], // Specify the routes the middleware applies to
 };
