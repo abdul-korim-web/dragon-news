d87ca41c421c587526a761de58d1d64542019df3
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-28 23:58:00 +0600

    categori list active button added

---
+++ src/components/CategoriItem.jsx
@@ -0,0 +1,16 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const CategoriItem = ({item}) => {
+    const pathName = usePathname()
+    const activeId = pathName.split("/")[2]
+    return (
+        <div>
+            <Link href={`/categorie/${item?.category_id}`} className={`w-[70%] text-[#9f9f9f] block text-xl font-medium  ${item?.category_id ==activeId ? "bg-blue-600 p-2 text-white rounded-2xl" :""}`}>{item?.category_name}</Link>
+        </div>
+    );
+}
+
+export default CategoriItem;
--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React, { use } from 'react';
+import CategoriItem from './CategoriItem';
     const getData = async()=>{
         const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
         const data =await res.json()
@@ -9,13 +10,14 @@ import React, { use } from 'react';
 const CategoriesList = async() => {
     // console.log(getCatrgorie());
     const data =await getData()
-    console.log('data', data)
+    // console.log('data', data)
     return (
         <div className='flix flex-col space-y-3'>
             <h2 className='text-[#403f3f] text-xl font-semibold '>All Categorie</h2>
             <ul className='flex flex-col space-y-5 '>
                 {data?.data?.news_category.map((item,index)=>(
-                    <Link href={`/categorie/${item?.category_id}`} className={`text-[#9f9f9f] text-xl font-medium `} key={index}>{item?.category_name}</Link>
+                    // <Link href={`/categorie/${item?.category_id}`} className={`text-[#9f9f9f] text-xl font-medium `} key={index}>{item?.category_name}</Link>
+                    <CategoriItem item={item} key={index}/>
                 ))}
             </ul>
         </div>
